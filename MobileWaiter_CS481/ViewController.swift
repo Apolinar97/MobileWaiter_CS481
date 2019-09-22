7553363745f67bb487cb02e3128a300054b29dd2
Apolinar Camacho <camacho.apolinar97@gmail.com>
2019-09-22 15:27:16 -0700

    Initial Commit

---
+++ MobileWaiter_CS481/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileWaiter_CS481
+//
+//  Created by Poly on 9/22/19.
+//  Copyright © 2019 Poly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
